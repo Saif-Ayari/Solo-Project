ef27a8bccc3e69e0a10a968ce9a0fc61de986211
Saif-Ayari <saifayari312@gmail.com>
2022-06-01 06:29:59 +0100

    backendfolder

--- server/index.js
+++ server/index.js
@@ -1,4 +1,22 @@
-import express from "express" ;
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
+
+
+import routes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors()) ;
+
+app.use('/posts', routes) 
+
+const connection_url = 'mongodb+srv://saif:123@cluster0.dhqnr.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+mongoose.connect(connection_url, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => app.listen(PORT, () => console.log(`server running on port : ${PORT} `))).catch((err) => console.log(err));
+
+
+
---
+++ server/models/PostBlog.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const postSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+})
+
+var PostBlog = mongoose.model('PostBlog', postSchema);
+
+export default PostBlog;
